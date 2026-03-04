d1c830f7cd02e51e609551ab714129da72599320
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-03-04 20:45:08 +0600

    feat: pronounceWord functionality added

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@
 const lessonBtnContainer = document.getElementById("lessonBtnContainer");
 const wordsContainer = document.getElementById("wordsContainer");
 
+//Load all Lessons Buttons
 const loadLessonBtns = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
     .then((json) => displayBtns(json.data));
 };
 
-
+//Display all Lessons Buttons
 const displayBtns = (btnList) => {
   lessonBtnContainer.innerHTML = "";
   for (let btn of btnList) {
@@ -22,12 +23,13 @@ const displayBtns = (btnList) => {
     lessonBtnContainer.appendChild(btnDiv);
   }
 };
-
+//Display all Lessons Buttons
 const loadLevelWord = (level_id) => {
-    fetch(`https://openapi.programming-hero.com/api/level/${level_id}`)
+  fetch(`https://openapi.programming-hero.com/api/level/${level_id}`)
     .then((res) => res.json())
     .then((json) => displayWords(json.data));
-}
+};
+//ShowDefault Message
 const showDefaultMessage = () => {
   wordsContainer.innerHTML = `
     <div class="col-span-full flex flex-col justify-center items-center text-center max-w-md mx-auto gap-4">
@@ -43,15 +45,14 @@ const showDefaultMessage = () => {
     </div>
   `;
 };
+//displayWords
 const displayWords = (wordList) => {
-    console.log()
-    wordsContainer.innerHTML = "";
-    if(wordList.length>0)
-    {
-        for(let word of wordList)
-        {
-            const wordDiv=document.createElement("div");
-            wordDiv.innerHTML =`
+  console.log();
+  wordsContainer.innerHTML = "";
+  if (wordList.length > 0) {
+    for (let word of wordList) {
+      const wordDiv = document.createElement("div");
+      wordDiv.innerHTML = `
                     <div class="bg-white rounded-xl text-center flex flex-col gap-6 sm:gap-8 p-6 sm:p-10 lg:p-14 w-full max-w-md mx-auto">
                         <div class="flex flex-col gap-4 sm:gap-6">
                             <h2 class="text-2xl sm:text-3xl font-bold text-gray-950">${word.word}</h2>
@@ -64,26 +65,39 @@ const displayWords = (wordList) => {
                                 <i class="fa-solid fa-circle-info text-slate-600 text-sm sm:text-base"></i>
                             </button>
                             <!-- Volume -->
-                            <button class="w-10 h-10 sm:w-12 sm:h-12 bg-sky-100 rounded-xl flex items-center justify-center cursor-pointer hover:bg-sky-200 transition">
-                                <i class="fa-solid fa-volume-high text-slate-600 text-sm sm:text-base"></i>
+                            <button onclick="pronounceWord('${word.word}','${word.id}')" class="w-10 h-10 sm:w-12 sm:h-12 bg-sky-100 rounded-xl flex items-center justify-center cursor-pointer hover:bg-sky-200 transition">
+                                <i id="volumeBtn-${word.id}" class="fa-solid fa-volume-high text-slate-600 text-sm sm:text-base"></i>
                             </button>
                         </div>
                     </div>
             `;
-            wordsContainer.appendChild(wordDiv);
-        }
+      wordsContainer.appendChild(wordDiv);
     }
-    else{
-            const wordDiv=document.createElement("div");
-            wordDiv.classList="col-span-full flex flex-col justify-center items-center text-center max-w-md mx-auto gap-4"
-            wordDiv.innerHTML =`
+  } else {
+    const wordDiv = document.createElement("div");
+    wordDiv.classList =
+      "col-span-full flex flex-col justify-center items-center text-center max-w-md mx-auto gap-4";
+    wordDiv.innerHTML = `
                     <img src="./assets/alert-error.png" alt="Alert Error">
                     <p class="text-gray-600"> এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি। </p>
                     <h1 class="text-2xl font-semibold">নেক্সট Lesson এ যান </h1>
             `;
-            wordsContainer.appendChild(wordDiv);
-    }
+    wordsContainer.appendChild(wordDiv);
+  }
+};
 
-}
+//Speaker Button Clicks
+const pronounceWord = (word, id) => {
+  const speech = new SpeechSynthesisUtterance(word);
+  speech.lang = "en-US";
+  speech.rate = 0.7;
+  speech.pitch = 1;
+  const volumeBtn = document.getElementById(`volumeBtn-${id}`);
+  volumeBtn.classList.add("animate-pulse");
+  speech.onend = () => {
+    volumeBtn.classList.remove("animate-pulse");
+  };
+  window.speechSynthesis.speak(speech);
+};
 loadLessonBtns();
 showDefaultMessage();
